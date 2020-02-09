92ffed7ec9786c1a2979b4e19f9d22237d7ec054
Leigh.Yu <leighyu1983@126.com>
2020-02-09 20:36:36 +0800

    algorithm

--- ley-single/src/main/java/com/ley/leetcode/algorithm/AlgoDoublePoints.java
+++ ley-single/src/main/java/com/ley/leetcode/algorithm/AlgoDoublePoints.java
@@ -12,6 +12,9 @@ public class AlgoDoublePoints {
 
     /**
      * 167. Two Sum II - Input array is sorted (Easy)
+     *
+     * Date: 2020-2-9
+     *
      * Input: numbers={2, 7, 11, 15}, target=9
      * Output: indexes [1, 2]
      *
@@ -52,6 +55,8 @@ public class AlgoDoublePoints {
     /**
      * 633. Sum of Square Numbers (Easy)
      *
+     * Date: 2020-2-9
+     *
      * 两数平方和
      * 可以看成是在元素为 0~target 的有序数组中查找两个数，使得这两个数的平方和为 target，如果能找到，则返回 true，表示 target 是两个整数的平方和。
      *
@@ -84,6 +89,8 @@ public class AlgoDoublePoints {
 
     /**
      * 345. Reverse Vowels of a String (Easy)
+     *
+     * Date: 2020-2-9
      *
      * 使用双指针，一个指针从头向尾遍历，一个指针从尾到头遍历，当两个指针都遍历到元音字符时，交换这两个元音字符。
      *
@@ -122,4 +129,71 @@ public class AlgoDoublePoints {
 
         return new String(result);
     }
+
+    /**
+     * 680. Valid Palindrome II (Easy)
+     *
+     * Date: 2020-2-9
+     *
+     * 题目：给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+     * 示例 1:
+     *
+     * 输入: “aba”
+     * 输出: True
+     * 示例 2:
+     *
+     * 输入: “abca”
+     * 输出: True
+     * 解释: 你可以删除c字符。
+     *
+     * 思路：验证回文串采用双指针，如果整体是回文串，直接true; 如果不是，那么就要根据题意分析，是左边删除一个还是右边删除一个。
+     * 所以需要写一个函数对左右在进行一次判断，只要有一个为true。那么说明删除一个元素后可以是回文子串。
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isValidPalindrome(String s) {
+        if(s == null) {
+            return false;
+        }
+
+        if(s.length() == 1) {
+            return true;
+        }
+
+        int i = 0, j = s.length() -1;
+
+        while(i < j) {
+            // 如果两侧的字符一致 当前是回文，左右指针挪动，判断下一次。
+            if(s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+                continue;
+            } else {
+              // 尝试删除一个字符(左右各式一次，只要满足就可以)，在测试回文。
+                return isValidPalindrome(s, ++i, j) || isValidPalindrome(s, i, --j);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 第二次判断
+     */
+    private static boolean isValidPalindrome(String s, int i, int j) {
+        while(i < j) {
+            // 如果两侧的字符一致 当前是回文，左右指针挪动，判断下一次。
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    
 }
