b296f90a7c531e0859e434bb2c5f8a6609377b9c
Leigh.Yu <Leigh.Yu@homecredit.cn>
2020-02-10 22:15:27 +0800

    多个pointcut派生类

--- ley-aop-config/src/main/java/com/ley/configuration/MyPointcutAdvisorConfigByBean.java
+++ ley-aop-config/src/main/java/com/ley/configuration/MyPointcutAdvisorConfigByBean.java
@@ -2,10 +2,13 @@ package com.ley.configuration;
 
 import com.ley.filter.MyKafkaFilter;
 import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.ClassFilter;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +25,6 @@ import org.springframework.context.annotation.Configuration;
 public class MyPointcutAdvisorConfigByBean {
     @Bean
     public AbstractPointcutAdvisor myAspectAdvisor() {
-
         return new AbstractPointcutAdvisor() {
             /**
              * 一个 method 执行前或执行后的动作
@@ -31,17 +33,37 @@ public class MyPointcutAdvisorConfigByBean {
              */
             @Override
             public Advice getAdvice() {
-                return null;
+                return new MethodInterceptor() {
+
+                    @Override
+                    public Object invoke(MethodInvocation invocation) throws Throwable {
+                        try {
+
+                            Object result = invocation.proceed();
+
+                            return result;
+                        } catch(Throwable e) {
+
+                            throw e;
+                        }
+                    }
+                };
             }
 
             /**
-             * 根据 method 的名字或者正则表达式去拦截一个 method
+             * 根据 class/method 的注解、名字或者正则表达式去拦截一个 method
              *
              * @return
              */
             @Override
             public Pointcut getPointcut() {
-                // 根据类注解，方法注解进行拦截
+                /**
+                 * 可以结合配置文件来选择用哪个类
+                 *
+                 * 注解切点: AnnotationMatchingPointcut 根据类注解，方法注解进行拦截
+                 * 表达式切点: AspectJExpressionPointcut
+                 *
+                 */
                 return new AnnotationMatchingPointcut(MyKafkaFilter.class, MyKafkaFilter.class);
             }
         };
